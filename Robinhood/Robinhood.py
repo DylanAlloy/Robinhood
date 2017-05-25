f9089be77c15a39b13d3f1274278ee97511c9c2b
Dylan Moore <hosnappp@gmail.com>
2017-05-25 10:37:32 -0400

    Update Robinhood.py

--- Robinhood/Robinhood.py
+++ Robinhood/Robinhood.py
@@ -10,10 +10,10 @@ from six.moves.urllib.parse import unquote
 from six.moves.urllib.request import getproxies
 from six.moves import input
 
-if six.PY3: #pragma: no cover
-    import exceptions as RH_exception
-else:       #pragma: no cover
-    import exceptions as RH_exception
+#if six.PY3: #pragma: no cover
+#    import exceptions as RH_exception 
+#else:       #pragma: no cover
+#    import exceptions as RH_exception
 
 class Bounds(Enum):
     """enum for bounds in `historicals` endpoint"""
